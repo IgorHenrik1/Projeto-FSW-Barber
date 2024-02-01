0c4d3c5fb042aca13af6691246e8b428f3a13c1c
Igor <igor.freitas@gruponsc.com.br>
2024-01-31 21:13:08 -0300

    feat: add initial booking component

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent } from '@/app/_components/ui/card';
+import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar';
+import { Badge } from '../components/ui/badge';
+
+const BookingItem = () => {
+    return (
+        <Card>
+            <CardContent className="p-5 flex justify-between py-0">
+                <div className="flex flex-col gap-2 py-5">
+                    <Badge className="bg-[#221c3d] text-primary hover:bg-[#221c3d] w-fit">
+                        Confirmado
+                    </Badge>
+                    <h2 className="font-bold">Corte de Cabelo</h2>
+                    <div className="flex items-center gap-2">
+                        <Avatar className="h-6 w-6">
+                            <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+                            <AvatarFallback>A</AvatarFallback>
+                        </Avatar>
+
+                        <h3 className="text-sm">Vintage Barber</h3>
+                    </div>
+                </div>
+
+                <div className="flex flex-col items-center justify-center border-l border-solid border-secondary px-3">
+                    <p>Fevereiro</p>
+                    <p className="text-sm">Fevereiro</p>
+                    <p className="text-2xl">06</p>
+                    <p className="text-sm">09:45</p>
+                </div>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default BookingItem;
