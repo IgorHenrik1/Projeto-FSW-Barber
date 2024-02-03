72f91dc4b4b09a1559aefde6df35d7ac32f187da
Igor <igor.freitas@gruponsc.com.br>
2024-02-03 16:06:15 -0300

    feat:  add initial service booking

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -38,6 +38,7 @@ const BarbershopDetailsPage = async ({
                 {barbershop.services.map((service) => (
                     <ServiceItem
                         key={service.id}
+                        barbershop={barbershop}
                         service={service}
                         isAuthenticated={!!session?.user}
                     />
