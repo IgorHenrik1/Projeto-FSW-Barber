3791897a112edad0292593a53f58ec0761ed25bb
Igor <igor.freitas@gruponsc.com.br>
2024-02-03 17:08:21 -0300

    feat:  add bookings

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -12,11 +12,12 @@ import {
     SheetTrigger,
 } from '@/app/_components/ui/sheet';
 import { Barbershop, Service } from '@prisma/client';
-import { format } from 'date-fns';
+import { format, setHours, setMinutes } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useMemo, useState } from 'react';
+import { saveBooking } from '../_actions/save-booking';
 import { generateDayTimeList } from '../_helpers/hours';
 
 interface ServiceItemProps {
@@ -30,6 +31,7 @@ const ServiceItem = ({
     barbershop,
     isAuthenticated,
 }: ServiceItemProps) => {
+    const { data } = useSession();
     const [date, setDate] = useState<Date | undefined>(undefined);
     const [hour, setHour] = useState<string | undefined>();
 
@@ -46,10 +48,28 @@ const ServiceItem = ({
         if (!isAuthenticated) {
             return signIn('google');
         }
-
-        //TODO: abrir modal de agendamento
     };
 
+    const handleBookingSubmit = async () => {
+        try {
+            if (!hour || !date || !data?.user) {
+                return;
+            }
+
+            const dateHour = Number(hour.split(':')[0]);
+            const dateMinutes = Number(hour.split(':')[1]);
+
+            const newDate = setMinutes(setHours(date, dateHour), dateMinutes);
+            await saveBooking({
+                serviceId: service.id,
+                barbershopId: barbershop.id,
+                date: newDate,
+                userId: (data.user as any).id,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
     const timeList = useMemo(() => {
         return date ? generateDayTimeList(date) : [];
     }, [date]);
@@ -219,7 +239,10 @@ const ServiceItem = ({
                                         </Card>
                                     </div>
                                     <SheetFooter className="px-5">
-                                        <Button disabled={!hour || !date}>
+                                        <Button
+                                            onClick={handleBookingSubmit}
+                                            disabled={!hour || !date}
+                                        >
                                             Confirmar reserva
                                         </Button>
                                     </SheetFooter>
